fe648c99fcf558a0c28af25e62f27bce6f9a2a3e
Olusanya Joy Naomi <91166707+joynaomi81@users.noreply.github.com>
2024-10-07 20:17:02 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle 
+import numpy as np 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+def load_tfidf():
+    tfidf = pickle.load(open("feature.pkt.ptk", "rb"))
+    return tfidf
+
+def load_model():
+    nb_model = pickle.load(open("toxic model.pkt.ptk", "rb"))
+    return nb_model
+
+def toxic_prediction(text):
+    tfidf = load_tfidf()
+    text_tfidf = tfidf.transform([text]).toarray()
+    nb_model = load_model()
+    prediction = nb_model.predict(text_tfidf)
+    class_name = "Toxic" if prediction == 1 else "Non-Toxic"
+    return class_name
+
+st.header("Toxic Comment Detection Web App")
+
+# Add your image above the result section
+image_path = "toxic...jpeg" 
+st.image(image_path, caption="Toxic Comment Detection", use_column_width=True)
+
+st.subheader("Input your text")
+text_input = st.text_input("Enter your comment:")
+
+if text_input is not None:
+    if st.button("Detect"):
+        result = toxic_prediction(text_input)
+        st.subheader("Result:")
+        st.info("The result is " + result + ".")
